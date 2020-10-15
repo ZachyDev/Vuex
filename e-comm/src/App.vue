fffa55f94a077cb6599628f107341c9f8fe46f43
ZachyDev <mosetizachary001@gmail.com>
2020-10-16 00:25:54 +0300

    Finished setting up a simple Vue app

---
+++ e-comm/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <product-list-one :products="products"/>
+    <product-list-two :products="products"/>
+  </div>
+</template>
+
+<script>
+import ProductListOne from '@/components/ProductListOne.vue';
+import ProductListTwo from '@/components/ProductListTwo.vue';
+
+export default {
+  name: 'App',
+  components: {
+    'product-list-one': ProductListOne,
+    'product-list-two': ProductListTwo
+  },
+  data() {
+    return {
+      products: [
+        { name: 'Laptop',price: 20000 },
+        { name: 'Mifi',price: 5000 },
+        { name: 'Router',price: 10000 },
+        { name: 'Zuku',price: 4000 },
+      ],
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ e-comm/src/components/ProductListOne.vue
@@ -0,0 +1,42 @@
+<template>
+    <div id="product-list-one">
+        <h1>Product List One</h1>
+        <ul>
+            <li v-for="(product,index) in products" :key="index">
+                <span class="name">{{ product.name }}</span>
+                <span class="price"> {{ product.price }}</span>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ProductListOne',
+    props: [ 'products' ]
+
+}
+</script>
+
+<style scoped>
+#product-list-one{
+    background: #FFF8B1;
+    box-shadow: 1px 2px 3px rgba(0,0,0,0.2);
+    margin-bottom: 30px;
+    padding: 10px 20px;
+}
+#product-list-one ul{
+    padding: 0;
+}
+#product-list-one li{
+    display: inline-block;
+    margin-right: 10px;
+    margin-top: 10px;
+    padding: 20px;
+    background: rgba(255,255,255,0.7);
+}
+.price{
+    font-weight: bold;
+    color: #E8800C;
+}
+</style>
